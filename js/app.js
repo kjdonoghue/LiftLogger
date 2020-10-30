b28da9bc4b649d1b672579f1dfd27852ee1c243e
ChapmanChappelle <chapman.chappelle@gmail.com>
2020-10-30 12:47:33 -0400

    began working on register function

--- js/app.js
+++ js/app.js
@@ -1,28 +1,42 @@
 /* CONSTANTS */
-const express = require('express')
+const express = require("express")
 const app = express()
 const PORT = 3000
-const pgp = require('pg-promise')()
-var bcrypt = require('bcryptjs');
-const connectionString = 'postgres://localhost:5432/mydatabase'
+const pgp = require("pg-promise")()
+var bcrypt = require("bcryptjs")
+const connectionString =
+   "postgres://skvpwhin:lXgkojz2TanCel7pEUUSDtGg-bEKm4NW@lallah.db.elephantsql.com:5432/skvpwhin"
 const db = pgp(connectionString)
-const mustacheExpress = require('mustache-express')
+const mustacheExpress = require("mustache-express")
+const { RSA_NO_PADDING } = require("constants")
 /* CONSTANTS */
 app.use(express.urlencoded())
-app.engine('mustache',mustacheExpress())
+app.engine("mustache", mustacheExpress())
 // the pages are located in views directory
-app.set('views','./views')
+app.set("views", "./views")
 // extension will be .mustache
-app.set('view engine','mustache')
+app.set("view engine", "mustache")
 
-
-app.get('/',(req,res) => {
-    res.render('index')
+app.get("/", (req, res) => {
+   res.render("index")
 })
 
+app.post('/register',(req,res) => {
 
+    const username = req.body.username 
+    const password = req.body.password 
+
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(password, salt, function(err, hash) {
+            db.none('INSERT INTO users(username, password) VALUES($1,$2)',[username, hash])
+            .then(() => {
+                res.redirect('/')
+            })
+        })
+    })
+})
 
 //LISTENER
 app.listen(PORT, () => {
-   console.log('Server is running...')
+   console.log("Server is running...")
 })
