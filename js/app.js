28a2047a2aab1393a87cdb070e8e5656876ca296
ChapmanChappelle <chapman.chappelle@gmail.com>
2020-10-30 13:10:25 -0400

    added user registration

--- js/app.js
+++ js/app.js
@@ -9,32 +9,39 @@ const connectionString =
 const db = pgp(connectionString)
 const mustacheExpress = require("mustache-express")
 const { RSA_NO_PADDING } = require("constants")
-/* CONSTANTS */
+/* CONSTANTS END*/
+
+/* CREATING VIEWS */
 app.use(express.urlencoded())
 app.engine("mustache", mustacheExpress())
 // the pages are located in views directory
 app.set("views", "./views")
 // extension will be .mustache
 app.set("view engine", "mustache")
+/* CREATING VIEWS END*/
 
+/* INITIAL PAGE */
 app.get("/", (req, res) => {
    res.render("index")
 })
+/* INITIAL PAGE END*/
 
-app.post('/register',(req,res) => {
-
-    const username = req.body.username 
-    const password = req.body.password 
-
-    bcrypt.genSalt(10, function(err, salt) {
-        bcrypt.hash(password, salt, function(err, hash) {
-            db.none('INSERT INTO users(username, password) VALUES($1,$2)',[username, hash])
-            .then(() => {
-                res.redirect('/')
-            })
-        })
-    })
+/* REGISTER FUNCTION */
+app.post("/register", (req, res) => {
+   let username = req.body.username
+   let password = req.body.password
+   bcrypt.genSalt(10, function (err, salt) {
+      bcrypt.hash(password, salt, function (err, hash) {
+         db.none("INSERT INTO users(username, password) VALUES($1,$2)", [
+            username,
+            hash,
+         ]).then(() => {
+            res.redirect("/")
+         })
+      })
+   })
 })
+/* REGISTER FUNCTION END*/
 
 //LISTENER
 app.listen(PORT, () => {
