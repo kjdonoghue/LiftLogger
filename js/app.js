3a4ca5fe2984a8593e682c21174f8ee221f0bc28
ChapmanChappelle <chapman.chappelle@gmail.com>
2020-11-02 12:18:03 -0500

    added login function

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ const connectionString =
    "postgres://skvpwhin:lXgkojz2TanCel7pEUUSDtGg-bEKm4NW@lallah.db.elephantsql.com:5432/skvpwhin"
 const db = pgp(connectionString)
 const mustacheExpress = require("mustache-express")
+const session = require('express-session')
 /* CONSTANTS END*/
 
 /* CREATING VIEWS */
@@ -25,12 +26,25 @@ app.get("/", (req, res) => {
 })
 /* INITIAL PAGE END*/
 
-/* USER PAGE */
-app.get("/user", (req,res) => {
-   res.render("sign-in")
+/******************* AUTHENTICATION STUFF *****************/
+// initalize the session
+app.use(express.urlencoded())
+app.use(session({
+   secret: "keyboard cat",
+   resave: false,
+   saveUnitialized: true
+}))
+// logging middleware
+function loggingMiddleware(req,res, next) {
+   console.log("LOGGING MIDDLEWARE")
+   next() // continue with the original request 
+}
+app.use(loggingMiddleware)
+// registration page action
+app.get("/register", (req, res) => {
+   res.render("register")
 })
-/* USER PAGE END*/
-/* REGISTER FUNCTION */
+// create user
 app.post("/register", (req, res) => {
    let username = req.body.username
    let password = req.body.password
@@ -38,18 +52,40 @@ app.post("/register", (req, res) => {
    let weight = req.body.weight
    bcrypt.genSalt(10, function (err, salt) {
       bcrypt.hash(password, salt, function (err, hash) {
-         db.none("INSERT INTO users(username, password, height, weight) VALUES($1,$2,$3,$4)", [
-            username,
-            hash,
-            height,
-            weight
-         ]).then(() => {
-            res.redirect("index")
+         db.none(
+            "INSERT INTO users(username, password, height, weight) VALUES($1,$2,$3,$4)",
+            [username, hash, height, weight]
+         ).then(() => {
+            res.redirect("/login")
          })
       })
    })
 })
-/* REGISTER FUNCTION END*/
+// show the login page
+app.get("/login", (req, res) => {
+   res.render("login")
+})
+// log in function
+app.post("/login", (req, res) => {
+   let username = req.body.username
+   let password = req.body.password
+
+   db.any("SELECT username, password FROM users").then((users) => {
+      users.forEach((element) => {
+         if (username == element.username) {
+            console.log("W")
+            bcrypt.compare(password, element.password).then(function (result) {
+               if (result == true) {
+                  res.render('login',{message: 'Username or password is correct'})
+               } else {
+                  res.render('login',{message: 'Username or password is incorrect'})
+               }
+            })
+         }
+      })
+   })
+})
+/******************* AUTHENTICATION STUFF *****************/
 
 //LISTENER
 app.listen(PORT, () => {
