c32df3d0baf320653af1c43e285978161ec8cd18
ChapmanChappelle <chapman.chappelle@gmail.com>
2020-11-02 15:29:02 -0500

    did a bunch of commenting and formatting

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express()
 const PORT = 3000
 const pgp = require("pg-promise")()
-var bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs")
 const connectionString =
    "postgres://skvpwhin:lXgkojz2TanCel7pEUUSDtGg-bEKm4NW@lallah.db.elephantsql.com:5432/skvpwhin"
 const db = pgp(connectionString)
@@ -11,21 +11,21 @@ const mustacheExpress = require("mustache-express")
 const session = require("express-session")
 /* CONSTANTS END*/
 
-/* CREATING VIEWS */
+/* VIEWS */
 app.use(express.urlencoded())
 app.engine("mustache", mustacheExpress())
 // the pages are located in views directory
 app.set("views", "./views")
 // extension will be .mustache
 app.set("view engine", "mustache")
-/* CREATING VIEWS END*/
+/* VIEWS END*/
 
 /***************************** AUTHENTICATION STUFF ***************************** */
-// initalize the session
+// initalize session
 app.use(express.urlencoded())
 app.use(
    session({
-      secret: "keyboard cat",
+      secret: "keyboard cat", // this needs to be changed
       resave: false,
       saveUnitialized: true,
    })
@@ -36,15 +36,20 @@ app.get("/register", (req, res) => {
 })
 // create user
 app.post("/register", (req, res) => {
+   // taking user inputs
    let username = req.body.username
    let password = req.body.password
    let height = req.body.height
    let weight = req.body.weight
+   // checks if the username already exists, if not continue, if it does page reloads with a "Username already exists" message
    db.any("SELECT username FROM users").then((users) => {
+      // loop through every username inside the users table
       users.forEach((element) => {
          if (username != element.username) {
+            // hash password
             bcrypt.genSalt(10, function (err, salt) {
                bcrypt.hash(password, salt, function (err, hash) {
+                  // 
                   db.none(
                      "INSERT INTO users(username, password, height, weight) VALUES($1,$2,$3,$4)",
                      [username, hash, height, weight]
@@ -67,10 +72,14 @@ app.get("/login", (req, res) => {
 })
 // log in function
 app.post("/login", (req, res) => {
+   // taking user input
    let username = req.body.username
    let password = req.body.password
+   // gets all user info from users table
    db.any("SELECT username, password, user_id FROM users").then((users) => {
+      // loop through users
       users.forEach((element) => {
+         // if username is correct, then check password accuracy with the compare function (bcryptjs). If password is incorrect, display "Username or password is incorrect"
          if (username == element.username) {
             loggedIn = bcrypt
                .compare(password, element.password)
@@ -86,11 +95,15 @@ app.post("/login", (req, res) => {
                      })
                   }
                })
+         } else {
+            res.render("login", {
+               message: "Username or password is incorrect",
+            })
          }
       })
    })
 })
-// authentication middleware
+// authentication middleware, redirects to login page if user doesnt exist in session
 function authenticate(req, res, next) {
    if (req.session) {
       if (req.session.username) {
@@ -103,7 +116,7 @@ function authenticate(req, res, next) {
       res.redirect("/login")
    }
 }
-// just a test page to see if middleware works
+// test page for middleware
 app.get("/testPage", authenticate, (req, res) => {
    let user = req.session.username
    res.render("test", { user: user })
