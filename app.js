7222e0d28bf670ec6be0ddf504a980b52e915aa0
ChapmanChappelle <chapman.chappelle@gmail.com>
2020-11-04 10:09:11 -0500

    formatting

--- app.js
+++ app.js
@@ -9,19 +9,18 @@ const connectionString =
 const db = pgp(connectionString)
 const mustacheExpress = require("mustache-express")
 const session = require("express-session")
-const path = require('path')
-const VIEWS_PATH = path.join(__dirname, './views' )
+const path = require("path")
+const VIEWS_PATH = path.join(__dirname, "./views")
 // require('dotenv').config()
 /* CONSTANTS END*/
-
 
 /* CREATING VIEWS */
 app.use(express.urlencoded())
 // app.engine("mustache", mustacheExpress())
-app.engine('mustache',mustacheExpress(VIEWS_PATH + '/partials', '.mustache'))
+app.engine("mustache", mustacheExpress(VIEWS_PATH + "/partials", ".mustache"))
 // the pages are located in views directory
 // app.set("views", "./views")
-app.set('views', VIEWS_PATH)
+app.set("views", VIEWS_PATH)
 // extension will be .mustache
 app.set("view engine", "mustache")
 /* CREATING VIEWS END*/
@@ -129,8 +128,7 @@ function authenticate(req, res, next) {
 app.get("/testPage", authenticate, (req, res) => {
    let user = req.session.username
    let uid = req.session.userId
-   res.render("test", { user: user, uid: uid})
-
+   res.render("test", { user: user, uid: uid })
 })
 /***************************** AUTHENTICATION STUFF ***************************** */
 /***************************** ROUTINE CREATOR STUFF ***************************** */
@@ -168,94 +166,109 @@ app.get("/routineCreator", (req, res) => {
    )
 })
 
-
 /***************************** ROUTINE CREATOR STUFF END ***************************** */
-
 
 /***************************** DASHBOARD STUFF ***************************** */
 /* Display Dashboard Page */
 app.get("/dashboard", async (req, res) => {
-    let id = 1
-    
-        let userHistory = await db.any('SELECT user_id FROM histories')   
+   let id = 1
 
-        let found = userHistory.find(user => {
-            return user.user_id == id
-        })       
+   let userHistory = await db.any("SELECT user_id FROM histories")
 
-        if (found) {
-            let result = await db.any('SELECT users.user_id, username, height, weight, workout_name, exercises FROM users JOIN histories ON users.user_id = histories.user_id WHERE users.user_id = $1 ORDER BY date_completed DESC LIMIT 7', [id])
-            let count = await db.any('SELECT COUNT (*) FROM histories WHERE user_id =$1', [id])
-            
-            user_dashboard = getUserDetails(result, count)
+   let found = userHistory.find((user) => {
+      return user.user_id == id
+   })
 
-            res.render('dashboard', {Dashboard: user_dashboard})
-            
-        } else {
-            let result = await db.any('SELECT users.user_id, username, height, weight FROM users WHERE user_id=$1', [id])
-            res.render('dashboard', {Dashboard: result})
-        }          
-    
+   if (found) {
+      let result = await db.any(
+         "SELECT users.user_id, username, height, weight, workout_name, exercises FROM users JOIN histories ON users.user_id = histories.user_id WHERE users.user_id = $1 ORDER BY date_completed DESC LIMIT 7",
+         [id]
+      )
+      let count = await db.any(
+         "SELECT COUNT (*) FROM histories WHERE user_id =$1",
+         [id]
+      )
+
+      user_dashboard = getUserDetails(result, count)
+
+      res.render("dashboard", { Dashboard: user_dashboard })
+   } else {
+      let result = await db.any(
+         "SELECT users.user_id, username, height, weight FROM users WHERE user_id=$1",
+         [id]
+      )
+      res.render("dashboard", { Dashboard: result })
+   }
 })
 
 //function for getting dashboard info with workouts
 function getUserDetails(result, count) {
-    
-    user_dashboard = []
+   user_dashboard = []
 
-    result.forEach((item) => {
-        if (user_dashboard.length == 0)  {
-            let information = {user_id: item.user_id, username: item.username, weight: item.weight, height: item.height, count: count, workouts: [{title: item.workout_name, exercises: item.exercises}]}
-            
-            user_dashboard.push(information)
-            
-        } else {
-            let information = user_dashboard.find(information => information.user_id == item.user_id)
-            if (information) {
-                information.workouts.push({title: item.workout_name, exercises: item.exercises}) 
-            } 
-        }
-        
-    })
-    return user_dashboard
+   result.forEach((item) => {
+      if (user_dashboard.length == 0) {
+         let information = {
+            user_id: item.user_id,
+            username: item.username,
+            weight: item.weight,
+            height: item.height,
+            count: count,
+            workouts: [{ title: item.workout_name, exercises: item.exercises }],
+         }
+
+         user_dashboard.push(information)
+      } else {
+         let information = user_dashboard.find(
+            (information) => information.user_id == item.user_id
+         )
+         if (information) {
+            information.workouts.push({
+               title: item.workout_name,
+               exercises: item.exercises,
+            })
+         }
+      }
+   })
+   return user_dashboard
 }
 /***************************** ROUTINES STUFF ***************************** */
 
 /* Routines Page */
 /* Display All Routines */
 app.get("/routines", (req, res) => {
-    db.any('SELECT workout_id, title, exercises FROM workouts')
-    .then(routines => {
-        res.render('routines', {allRoutines: routines})
-    })
+   db.any("SELECT workout_id, title, exercises FROM workouts").then(
+      (routines) => {
+         res.render("routines", { allRoutines: routines })
+      }
+   )
 })
 
 app.post("/delete-routine", (req, res) => {
-    let workout_id = req.body.workout_id
+   let workout_id = req.body.workout_id
 
-    db.none('DELETE FROM workouts WHERE workout_id=$1', [workout_id])
-    .then(() => {
-        res.redirect('routines')
-    })
-   
+   db.none("DELETE FROM workouts WHERE workout_id=$1", [workout_id]).then(
+      () => {
+         res.redirect("routines")
+      }
+   )
 })
 
 /***************************** DASHBOARD AND ROUTINES STUFF ***************************** */
 
 /* Go from Dashboard or Routines to Workout page after selecting workout*/
 app.post("/select", (req, res) => {
-    let workout_id = req.body.workout_id
-    
-    db.any('SELECT workout_id, title, exercises FROM workouts WHERE workout_id= $1', [workout_id])
-    .then(workout => {
-      
-        res.render('workout', {selectedWorkout: workout})
-    })
+   let workout_id = req.body.workout_id
 
+   db.any(
+      "SELECT workout_id, title, exercises FROM workouts WHERE workout_id= $1",
+      [workout_id]
+   ).then((workout) => {
+      res.render("workout", { selectedWorkout: workout })
+   })
 })
 
 /***************************** DASHBOARD AND ROUTINES STUFF END ***************************** */
 
 app.listen(PORT, () => {
-   console.log('Server is running...')
+   console.log("Server is running...")
 })
